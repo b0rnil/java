6a03ce9424a79ac9f0dfe83bb7baa2fc0a33dbb2
b0rnil <bornilgogoi03@gmail.com>
2026-02-03 11:17:21 +0530

    Swap Value

--- src/aClassCodes/VarAndDataTypes/SwapWithout3rdVar.java
+++ src/aClassCodes/VarAndDataTypes/SwapWithout3rdVar.java
@@ -1,5 +1,14 @@
 package aClassCodes.VarAndDataTypes;
 
 public class SwapWithout3rdVar {
-    
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println("Before swap: a = " + a + ", b = " + b);
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("After swap: a = " + a + ", b = " + b);
+    }
 }
