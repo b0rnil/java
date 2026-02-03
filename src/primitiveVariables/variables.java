45132f68a78760569a8f57dc62d0e798da67b90e
b0rnil <bornilgogoi03@gmail.com>
2026-02-03 08:12:36 +0530

    Basic Java code with variables

--- src/primitiveVariables/variables.java
+++ src/primitiveVariables/variables.java
@@ -1,7 +1,16 @@
 package primitiveVariables;
 
-public class variables {
+public class Variables {
     public static void main(String[] args) {
-        
+        int age = 25;
+        float temperature = 36.5f;
+        char grade = 'A';
+        boolean isStudent = true;
+
+        //Display
+        System.out.println("Age: " + age);
+        System.out.println("Temperature: " + temperature);
+        System.out.println("Is a student: " + isStudent);
+        System.out.println("Grade: " + grade);
     }
 }
