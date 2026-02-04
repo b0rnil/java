c2d94b26bf003180f85feca9a481d870061b8e18
b0rnil <bornilgogoi03@gmail.com>
2026-02-04 12:47:30 +0530

    Inches to Meters java code

--- src/aClassCodes/VarAndDataTypes/InchesToMeter.java
+++ src/aClassCodes/VarAndDataTypes/InchesToMeter.java
@@ -1,7 +1,17 @@
 package aClassCodes.VarAndDataTypes;
+import java.util.*;
 
 public class InchesToMeter {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter inches: ");
+        double inches = sc.nextDouble();
+
+        double meter = inches*0.0254;
+
+        System.out.println("Meters: " + meter);
+
+        sc.close();
     }
 }
