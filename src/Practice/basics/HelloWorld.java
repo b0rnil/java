e9623e7c3e17bafc681fae584facfacded4383bc
b0rnil <bornilgogoi03@gmail.com>
2026-02-05 01:41:53 +0530

    Restructuring Folders

--- src/Practice/basics/HelloWorld.java
+++ src/Practice/basics/HelloWorld.java
@@ -1,4 +1,4 @@
-package basics;
+package Practice.basics;
 
 public class HelloWorld {
     public static void main(String[] args) {
--- src/Practice/primitiveVariables/Variables.java
+++ src/Practice/primitiveVariables/Variables.java
@@ -1,4 +1,4 @@
-package primitiveVariables;
+package Practice.primitiveVariables;
 
 public class Variables {
     public static void main(String[] args) {
